73c9bd65ed915ed46395510dc2c2d2c8075ebac1
Caio Maqueise Alécio Pinheiro <caio.pinheiro@ifms.edu.br>
2025-07-14 15:04:03 -0400

    first commit

---
+++ src/api/deck-of-cards.js
@@ -0,0 +1,12 @@
+ export async function criarBaralho(){
+    const response = await
+    fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    const baralho = await response.json()
+    return baralho
+}
+ export async function comprarCarta(baralhoId){
+    const response = await
+    fetch(`https://www.deckofcardsapi.com/api/deck/${baralhoId}/draw/?count=1`)    
+    const cartasCompradas = await response.json()
+    return cartasCompradas
+}
---
+++ src/deck-card.js
@@ -0,0 +1,32 @@
+import "./style.css"
+import { comprarCarta, criarBaralho } from "./api/deck-of-cards"
+import { Coins, createIcons, Dices, Layers } from "lucide"
+createIcons({
+    icons: {
+        Dices,
+        Layers,
+        Coins
+    }
+})
+
+const btnNovoBaralho = document.querySelector("#btnNovoBaralho")
+const btnResetarMesa = document.querySelector("#btnResetarMesa")
+const baralhoId = document.querySelector("#baralhoId")
+const cartasRestantes = document.querySelector("#cartasRestantes")
+const mesaBaralho = document.querySelector("#mesaBaralho")
+const mesaCartas = document.querySelector("#mesaCartas")
+
+btnNovoBaralho.addEventListener("click", async function (event) {
+    const baralho = await criarBaralho()
+    localStorage.setItem("baralhoId", baralho.deck_id)
+    const btn = event.target
+    btn.setAttribute("disabled", true)
+    baralhoId.textContent = baralho.deck_id
+    cartasRestantes.textContent = baralho.remaining
+})
+mesaBaralho.addEventListener("click", async function () {
+    const cartasCompradas = await comprarCarta(
+        localStorage.getItem("baralhoId")
+    )
+    console.log(cartasCompradas)
+})
