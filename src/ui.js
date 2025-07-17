ecd9a5df33e57b379532a2712409d67f11aa4c5d
Caio Maqueise Alécio Pinheiro <caio.pinheiro@ifms.edu.br>
2025-07-17 20:14:01 +0000

    refatorar: renomear função criarBaralhoUI para renderizarBaralhoMesa e ajustar lógica de renderização

--- src/ui.js
+++ src/ui.js
@@ -1,17 +1,20 @@
-export function criarBaralhoUI() {
-    const mesaBaralho = document.querySelector("#mesaBaralho");
+function renderizarBaralhoMesa() {
+    const mesaBaralho = document.querySelector("#mesaBaralho")
     mesaBaralho.innerHTML = ""; // Limpa o conteúdo anterior
-    const baralho = document.createElement("div");
-    baralho.id = "baralho";
-    baralho.className = "stack w-24";
-
-    for (let i = 0; i < 10; i++) {
-        const img = document.createElement("img");
-        img.src = "https://deckofcardsapi.com/static/img/back.png";
-        img.className = "w-24 slide-in-left";
-        img.style.animationDelay = `${0.5 - i * 0.1}s`; // Ajusta o atraso da animação
-        baralho.appendChild(img);
+    const baralho = document.createElement("div")
+    baralho.id = "baralho"
+    baralho.classList.add('w-24', 'stack')
+    for (let i = 1; i <= 20; i++) {
+        const carta = document.createElement("img")
+        carta.classList.add('carta', 'slide-in-left')
+        carta.src = 'https://www.deckofcardsapi.com/static/img/back.png'
+        carta.style.animationDelay = `${0.5 - i * 0.1}s`; // Ajusta o atraso da animação
+        baralho.appendChild(carta);
     }
-
+    mesaBaralho.appendChild(baralho);
+    baralho.firstChild?.classList.add('hover:scale-110', 'transition-transform', 'duration-300', 'ease-in-out', 'cursor-pointer');
     mesaBaralho.appendChild(baralho);
 }
+export const UI = {
+    renderizarBaralhoMesa,
+};
